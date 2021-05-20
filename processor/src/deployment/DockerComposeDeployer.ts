db32826ed19bd71351d29b9887036080d3db7b34
Peter Kullmann <peter.kullmann@iteratec.com>
2021-05-20 16:41:30 +0200

    Added handlebars for compose deployer

--- processor/src/deployment/DockerComposeDeployer.ts
+++ processor/src/deployment/DockerComposeDeployer.ts
@@ -1,12 +1,21 @@
+import * as fs from "fs";
+
 import {MicrozooDeployer} from "./MicrozooDeployer";
 import {MicrozooSystem} from "../model/MicrozooSystem";
+const Handlebars = require("handlebars");
 
 export class DockerComposeDeployer implements MicrozooDeployer {
+    private template;
+
     public constructor(private microzooSystem: MicrozooSystem) {
-        console.log("Deploying...");
+        const templateRaw = fs.readFileSync("./src/deployment/template/compose.hbs").toString();
+        console.log(templateRaw);
+        this.template = Handlebars.compile(templateRaw);
     }
 
     public deploy(): boolean {
+        console.log("Deploying...");
+        console.log(this.template({message: "world"}));
         return true;
     }
 }
--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,14 +1,7 @@
 import {DeployerFactory} from "./deployment/MicrozooDeployer";
-
-const PlantUmlToJson = require('puml2json');
-const path = require('path');
-
 import {Command, Option} from 'commander';
-
-import PumlValidator from "./validation/PumlValidator";
-import PumlTransformer from "./transformation/PumlTransformer";
-import MicrozooValidator from "./validation/MicrozooValidator";
 import {DockerComposeDeployer} from "./deployment/DockerComposeDeployer";
+import deploy from "./action/deploy";
 
 function start(argv) {
     const program = new Command();
@@ -26,29 +19,6 @@ function start(argv) {
             .catch(reason => console.log(reason));
       });
     program.parse(argv);
-}
-
-async function deploy(source, sourceFolder, target) {
-    const filePath = getFilePath(sourceFolder, source);
-    const converter = PlantUmlToJson.fromFile(filePath);
-    const pumlSystem = await converter.generate();
-    console.log(`- Parsing puml file '${filePath}' succeeded`);
-    let success = new PumlValidator(pumlSystem).validate();
-    console.log(`- Formal Validation ${success ? "succeeded" : "failed"}`);
-    const microzooSystem = new PumlTransformer(pumlSystem).transform();
-    console.log(`- Transformation ${success ? "succeeded" : "failed"}`);
-    success = new MicrozooValidator(microzooSystem).validate();
-    console.log(`- Semantic Validation ${success ? "succeeded" : "failed"}`);
-    success = DeployerFactory.get(target, microzooSystem).deploy();
-    console.log(`- Deployment ${success ? "succeeded" : "failed"}`);
-}
-
-function getFilePath(sourceFolder: string, source: string): string {
-    if (!source.match(/.*\.puml/)) {
-        source += ".puml";
-    }
-
-    return path.join(sourceFolder, source);
 }
 
 try {
